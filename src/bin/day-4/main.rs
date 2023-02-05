e05a5ef780f52501949d2f171b4c75d64a56f4a8
Miles Crighton <miles.crighton@gmail.com>
2023-02-05 11:40:08 +0000

    Day 4 - Part 2

--- src/bin/day-4/main.rs
+++ src/bin/day-4/main.rs
@@ -3,6 +3,10 @@ mod utils;
 use std::path::Path;
 use utils::read_lines;
 
+fn within_range(r1: &Vec<i32>, r2: &Vec<i32>) -> bool {
+    return r1[0] >= r2[0] && r1[0] <= r2[1] || r1[1] >= r2[0] && r1[1] <= r2[1];
+}
+
 fn main() {
     let path = Path::new("src/bin/day-4/input.txt");
     let lines = read_lines(path);
@@ -21,9 +25,7 @@ fn main() {
             .map(|a| a.parse().unwrap())
             .collect();
 
-        if schedule_1[0] >= schedule_2[0] && schedule_1[1] <= schedule_2[1]
-            || schedule_2[0] >= schedule_1[0] && schedule_2[1] <= schedule_1[1]
-        {
+        if within_range(&schedule_1, &schedule_2) || within_range(&schedule_2, &schedule_1) {
             total_score += 1;
         }
     }
